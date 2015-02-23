b1c846a302800da185e59e1437a58ed3768d2d71
Derek Ford <inconceivablevizzini@gmail.com>
2015-02-23 12:48:17 -0600

    Obeying Django inheritance rules, changed to using created vs uploaded for date_created

--- file_storage/models.py
+++ file_storage/models.py
@@ -6,12 +6,14 @@ fs_store = FileSystemStorage(location=settings.DOCUMENT_ROOT)
 fs_image_store = FileSystemStorage(location=settings.IMAGE_DOCUMENT_ROOT)
 
 class StoredFile(models.Model):
-    file = models.FileField(storage=fs_store)
-    date_uploaded = models.DateTimeField()
+    date_created = models.DateTimeField()
     date_modified = models.DateTimeField()
 
     class Meta:
         abstract = True
+
+class BinaryFile(StoredFile):
+    file = models.FileField(storage=fs_store)
 
 class ImageFile(StoredFile):
     file = models.ImageField(storage=fs_image_store)
