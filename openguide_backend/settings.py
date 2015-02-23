4799af22a5948ff22ecb5db34f19095431307f58
zroehr <zr5@rice.edu>
2015-02-23 10:32:51 -0600

    Refactoring models

--- openguide_backend/settings.py
+++ openguide_backend/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_guide'
+    'file_storage',
+    'rest_guide',
 ]
 
 MIDDLEWARE_CLASSES = [
