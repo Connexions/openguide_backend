620cb8f79b80f89998850fb90a44ed81ed028b10
zroehr <zr5@rice.edu>
2015-03-13 14:47:31 -0500

    Refactored API using querysets, viewsets, user authenication and hyperlinked serializers. Moved API views from rest_guide/api.py to rest_guide/views.py

--- openguide_backend/urls.py
+++ openguide_backend/urls.py
@@ -2,41 +2,20 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_guide import api
+from rest_framework import routers
+from rest_guide import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'themes', views.ThemeViewSet)
+router.register(r'books', views.BookViewSet)
+router.register(r'elements', views.ElementViewSet)
 
-    url(r'^api/elements/$', api.ElementList.as_view()),
-    url(r'^api/elements/(?P<pk>[0-9]+)/$', api.ElementDetail.as_view()),
 
+urlpatterns = [
+    url(r'^v1/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
-)
-urlpatterns = format_suffix_patterns(urlpatterns)
-# 
-# 
-# 
-# from django.conf.urls import include, url
-# from rest_framework import routers
-# from openguide_backend.rest_guide import views
-# #from django.contrib import admin
-# #from django.contrib.auth.models import User
-# #from rest_framework import router, serializers, viewsets
-# 
-# router = router.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
-# 
-# # Wire up our API using automatic URL routing.
-# # Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-#     # Examples:
-#     # url(r'^$', 'openguide_backend.views.home', name='home'),
-#     # url(r'^blog/', include('blog.urls')),
-#     url(r'^', include(router.urls)),
-#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-#     url(r'^', include('rest_guide.urls')),
-#     url(r'^admin/', include(admin.site.urls)),
-# ]
+
+]
--- rest_guide/views.py
+++ rest_guide/views.py
@@ -1,19 +1,42 @@
 from django.shortcuts import render
-# from django.contrib.auth.models import User, Group
-# from rest_framework import viewsets
-# from openguide_backend.rest_guide.serializers import UserSerializer, GroupSerializer
-# 
-# # Create your views here.
-# class UserViewSet(viewsets.ModelViewSet):
-#   """
-#   API endpoint that allows users to be viewed or edited.
-#   """
-#   queryset = User.objects.all()
-#   serializer_class = UserSerializer
-#   
-#   class GroupViewSet(viewsets.ModelViewSet):
-#   """
-#   API endpoint that allows groups to be viewed or edited.
-#   """
-#   queryset = Group.objects.all()
-#   serializer_class = GroupSerializer
+from django.contrib.auth.models import User, Group
+from .models import *
+from rest_framework import viewsets
+from .serializers import *
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class ThemeViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows themes to be viewed or edited
+    """
+    queryset = Theme.objects.all()
+    serializer_class = ThemeSerializer
+    
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows books to be viewed or edited
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class ElementViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows elements to be viewed or edited
+    """
+    queryset = Element.objects.all()
+    serializer_class = ElementSerializer
+
+
